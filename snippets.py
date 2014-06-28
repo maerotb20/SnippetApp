3de3e92e9e04634dcf62a94ed96ddbdc6658acba
maerotb20 <maerotb20@gmail.com>
2014-06-28 16:28:47 -0400

    2nd commit

--- snippets.py
+++ snippets.py
@@ -1,5 +1,7 @@
 import logging
 import csv
+import sys
+import argparse
 
 # Set the log output file, and the log level
 logging.basicConfig(filename="output.log", level=logging.DEBUG)
@@ -16,6 +18,35 @@ def put(name, snippet, filename):
     logging.debug("Write successful")
     return name, snippet
 
+
+def make_parser():
+    """ Construct the command line parser """
+    logging.info("Construction parser")
+    description = "Store and retrieve snippets of text"
+    parser = argparse.ArgumentParser(description=description)
+
+    subparsers = parser.add_subparsers(help="Available commands")
+
+    # Subparser for the put command
+    logging.debug("Constructing put subparser")
+    put_parser = subparsers.add_parser("put", help="Store a snippet")
+    put_parser.add_argument("name", help="The name of the snippet")
+    put_parser.add_argument("snippet", help="The snippet text")
+    put_parser.add_argument("filename", defaults="snippets.csv", nargs="?",
+                            help="The snippet filename")
+    put_parser.set_defaults(command="put")
+
+    return parser
+
+
+def main():
+    """Main function"""
+    logging.info("Starting snippets")
+    parser = make_parser()
+    arguments = parser.parse_args(sys.argv[1:])
+
+if __name__ == "__main__":
+    main()
 
 
 
